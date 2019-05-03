092983c06c5c18aeabf521aee58dc3adda0df8b3
3daddict <print@3daddict.com>
2019-05-03 13:44:56 -0700

    load file iteration array function

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const fs = require('fs');
 const members350000 = require('./input/membersListArray(350000).json');
+const members360000 = require('./input/membersListArray(360000).json');
 
-const scrubJsonUserList = (jsonFile) => {
+const JSONfiles = [
+    members350000,
+    members360000
+]
+
+const iterateFiles = (fileObject) => {
+    const loadFileObject = fileObject
+    let fileName = 10000;
+
+    for(let i = 0; i < loadFileObject.length; i++){
+        scrubJsonUserList(loadFileObject[i], fileName);
+        fileName = fileName + 10000;
+    }
+}
+
+const scrubJsonUserList = (jsonFile, fileName) => {
     const loadJsonFile = jsonFile;
 
     const dataToKeep = loadJsonFile.filter((item) => {
         return item.unsubscribe_reason === "N/A (Unsubscribed by admin)"
     })
-
-    console.log(dataToKeep);
-    
-
+    saveJsonFile(dataToKeep, fileName);
 }
 
-const saveJsonFile = (jsonFile) => {
-    const dataJSON = JSON.stringify(notes);
-    fs.writeFileSync('./output/', dataJSON);
+const saveJsonFile = (jsonFile, fileName) => {
+    const dataJSON = JSON.stringify(jsonFile);
+    fs.writeFileSync('./output/json/' + fileName + '.json', dataJSON);
 }
 
-scrubJsonUserList(members350000);
+iterateFiles(JSONfiles);
